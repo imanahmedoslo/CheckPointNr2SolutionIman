6be9ce5d50443e06a444da623c90bed97563af92
Iman Ahmed <imanahmedimanahmed@gmail.com>
2023-11-13 09:01:21 +0100

    made class name

---
+++ CheckPointNr2/Player.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CheckPointNr2
+{
+    /*
+     * Class: Player
+       Properties: Name, Number
+     */
+    internal class Player
+    {
+       
+        public string Name { get; } 
+          /*  { 
+                return _name;
+            } private set 
+            { 
+                if (value == null || value == string.Empty)
+                {
+                   // throw new Exception("Please give a proper name to the player.");
+                    _name = "ukjent navn.";
+                }
+                else { _name = value; }
+               
+            } 
+        }*/
+        public int Number { get; set; }
+        public Player(string name, int number) 
+        {
+            Name = name;
+            Number = number;
+
+        }
+    }
+}
